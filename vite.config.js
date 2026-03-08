b9072f48aa18ba5123682212e225579306327f89
Brendon05 <brendonlin17@hotmail.com>
2026-03-08 16:03:28 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/tedx-website-2026/' : '/',
+export default defineConfig({
+  base: '/tedx-website-2026/',
   plugins: [react(), tailwindcss()],
-}))
+})
